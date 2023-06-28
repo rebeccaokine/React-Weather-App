c39d9f42226765dd4d9a89d9e077f2054095c808
Bex <rebeccaokine.na@gmail.com>
2023-06-28 14:31:56 +0000

    update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Weather />
-      Open source code by <a href="https://github.com/rebeccaokine/React-Weather-App">Rebecca Okine</a>
+       <a href="https://github.com/rebeccaokine/React-Weather-App">Open source code by</a>Rebecca Okine
     </div>
   );
 }
